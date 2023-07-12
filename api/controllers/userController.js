86960347c22d9eeb314a4cb9d75696df645ce753
maxence.nho <maxence.nho@hetic.net>
2023-07-12 10:27:49 +0200

    Register / Login sécurisé

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -1,6 +1,7 @@
 import pool from '../config/db.js';
 import jwt from 'jsonwebtoken';
 import crypto from 'crypto'
+import bcrypt from 'bcryptjs'
 
 const jwtKey = "saline_secret_key"
 const jwtExpirySeconds = 300
@@ -10,29 +11,38 @@ const jwtExpirySeconds = 300
 export function login(req, res) {
   const { email, password } = req.body;
   if (email && password) {
+    console.log(email, password)
     try {
-      console.log(email, password)
       pool
         .query(
-          `SELECT id, firstName, lastName FROM users WHERE email='${email}' AND password='${password}' LIMIT 1`
+          `SELECT id, firstName, lastName, password FROM users WHERE email='${email}' LIMIT 1`
         )
         .then((response) => {
           if (response.length > 0) {
-            const token = jwt.sign(
-              { id: response[0].id }, jwtKey, {
-                algorithm: "HS256",
-                expiresIn: jwtExpirySeconds,
-              }
-            );
-            console.log("token : " + token)
-            res.cookie('access_token', token);
-            res.status(202).json({
-              status: 'Success',
-              user: {
-                lastName: response[0].lastName,
-                firstName: response[0].firstName,
-              },
-              token: token,
+            bcrypt.compare(password, response[0].password).then((bcryptRes) => {
+                if(bcryptRes == true){
+                  const token = jwt.sign(
+                    { id: response[0].id }, jwtKey, {
+                      algorithm: "HS256",
+                      expiresIn: jwtExpirySeconds,
+                    }
+                  );
+                  console.log("token : " + token)
+                  res.cookie('access_token', token);
+                  res.status(202).json({
+                    status: 'Success',
+                    user: {
+                      lastName: response[0].lastName,
+                      firstName: response[0].firstName,
+                    },
+                    token: token,
+                  });
+                }else{
+                  res.status(404).json({
+                    status: 'Failed',
+                    message: 'Email or password is incorrect',
+                  });
+                }
             });
           } else {
             res.status(404).json({
@@ -57,20 +67,21 @@ export function login(req, res) {
 
 export function register(req, res) {
   const { lastName, firstName, email, password, passwordConfirm } = req.body;
-
   if (email && firstName && lastName && password && passwordConfirm) {
     if (password == passwordConfirm) {
-      try {
-        pool
-          .query(`INSERT INTO users(email, lastname, firstname, password) VALUES('${email}', '${lastName}', '${firstName}', '${password}')`)
-      }
-      catch (e){
-        res.status(400).json({
-          status: 'Failed',
-          message: 'Request failed on INSERT',
-        });
-      }
-      login(req, res)
+      bcrypt.hash(password, 10, function(err, hash) {
+        try {
+          pool
+            .query(`INSERT INTO users(email, lastname, firstname, password) VALUES('${email}', '${lastName}', '${firstName}', '${hash}')`)
+        }
+        catch (e){
+          res.status(400).json({
+            status: 'Failed',
+            message: 'Request failed on INSERT',
+          });
+        }
+        login(req, res)
+      });
     } else {
       res.status(400).json({
         status: 'Failed',
