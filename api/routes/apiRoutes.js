a029f36f712f25dafc4f718f79b4b5d93cbd0879
maxence.nho <maxence.nho@hetic.net>
2023-07-13 13:27:42 +0200

    Only PNG JPEG JPG

--- api/routes/apiRoutes.js
+++ api/routes/apiRoutes.js
@@ -23,41 +23,60 @@ router.post('/user/register', register)
 
 
 const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, 'uploads/');
-  },
-  filename: (req, file, cb) => {
-    cb(null, Date.now() + '-' + file.originalname);
-  },
-});
-
-const upload = multer({ storage });
-
-// Route POST pour l'upload d'image
-router.post('/user/upload', upload.single('image'), (req, res) => {
-  if (req.file) {
-    console.log(req.file)
-    var typeOfFile = req.file.originalname.split('.')
-    console.log(typeOfFile)
-    console.log(typeOfFile[typeOfFile.length - 1])
-    if(typeOfFile[typeOfFile.length - 1] == "jpg" || typeOfFile[typeOfFile.length - 1] == "png"){
-        res.status(200).json({
-            status: 'Success'
-        });
-    }else{
-        res.status(400).json({
-            status: 'Failed',
-            message: "Type of file is wrong",
-        });
+    destination: (req, file, cb) => {
+      cb(null, 'uploads/');
+    },
+    filename: (req, file, cb) => {
+      cb(null, Date.now() + '-' + file.originalname);
+    },
+  });
+  
+  const upload = multer({
+    storage,
+    fileFilter: (req, file, cb) => {
+      const allowedMimeTypes = ['image/jpeg', 'image/png'];
+  
+      if (allowedMimeTypes.includes(file.mimetype)) {
+        cb(null, true);
+      } else {
+        cb(new Error('Invalid file type. Only JPEG and PNG images are allowed.'));
+      }
+    },
+  });
+  
+  // Middleware d'erreur personnalisé pour capturer les erreurs de téléchargement
+  const handleUploadError = (err, req, res, next) => {
+    if (err instanceof multer.MulterError) {
+      // Erreur de Multer lors du téléchargement de l'image
+      res.status(400).json({ error: 'Image upload failed.' });
+    } else {
+      // Erreur inattendue
+      res.status(500).json({ error: 'Internal server error.' });
     }
-    // Traitez le fichier, par exemple, enregistrez-le en base de données ou déplacez-le vers un emplacement permanent
-  } else {
-    res.status(400).json({
-        status: 'Failed',
-        message: "No image file found.",
+  };
+
+
+
+router.post('/user/upload', (req, res, next) => {
+    upload.single('image')(req, res, (err) => {
+      if (err) {
+        handleUploadError(err, req, res, next);
+      } else {
+        // Fichier envoyé accessible via req.file
+        if (req.file) {
+          // Traitez le fichier, par exemple, enregistrez-le en base de données ou déplacez-le vers un emplacement permanent
+          res.status(200).json({
+                status: 'Success'
+            });
+        } else {
+            res.status(400).json({
+                status: 'Failed',
+                message: "Type of file is wrong",
+            });
+        }
+      }
     });
-  }
-});
+  });
 
 //export this router to use in our index.js
 export default router
